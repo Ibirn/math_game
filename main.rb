4174e2febf033e50f54fc141c5197dd09087ce43
Ibirn <kmcricciuti@gmail.com>
2020-10-12 21:04:48 +0000

    Add opening prompt to start game

--- main.rb
+++ main.rb
@@ -1,2 +1,9 @@
 require './game.rb'
 run_game = Game.new
+puts "Welcome to the Number Game!"
+puts "Players take turns solving math problems."
+puts "If you get one wrong, you lose a life."
+puts "When one player loses all three of their lives, the game is over."
+puts "Are you ready to play?"
+gets.chomp
+run_game.start_round
