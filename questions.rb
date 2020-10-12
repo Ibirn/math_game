a109ae1da4305954e1b401b2191cf16a50be25b8
Ibirn <kmcricciuti@gmail.com>
2020-10-12 21:04:29 +0000

    Add scaling difficulty

--- questions.rb
+++ questions.rb
@@ -2,14 +2,29 @@ class Question
 
   attr_accessor :answer
 
-  def initialize
+  def initialize(difficulty)
     @num1 = rand(20)
     @num2 = rand(20)
-    @answer = @num1 + @num2
+    @difficulty = difficulty
   end
 
   def ask_question(player)
-    p "#{player}: What is #{@num1} + #{@num2}?"
+    if @difficulty < 5
+      @answer = @num1 + @num2
+      puts "#{player}: What is #{@num1} + #{@num2}?"
+    elsif @difficulty < 10
+      @answer = @num1 - @num2
+      puts "#{player}: What is #{@num1} - #{@num2}?"
+    elsif @difficulty < 15
+      @answer = @num1 * @num2
+      puts "#{player}: What is #{@num1} * #{@num2}?"
+    else
+      @num1 = rand(10)
+      @num2 = rand(10)
+      @num3 = rand(20)
+      @answer = @num1 * (@num2 / @num3)
+      puts "#{player}: What is #{@num1} * (#{@num2} + #{@num3})?"
+    end
   end
   
 end
